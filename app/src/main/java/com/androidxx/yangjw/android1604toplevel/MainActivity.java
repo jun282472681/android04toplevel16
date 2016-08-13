43a1193ce4261bfaba4e37cdc6a28b4a60d04c54
yangjw <yangwen8701@126.com>
2016-08-13 15:19:53 +0800

    张三：第二次提交MainActivity，在MainActivity中添加了一段代码

--- app/src/main/java/com/androidxx/yangjw/android1604toplevel/MainActivity.java
+++ app/src/main/java/com/androidxx/yangjw/android1604toplevel/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //zhangsan
+        //zhangsan2016
+        //2016
     }
 }
