95ccf8128225758855a3296f23750758301ad3f5
yangjw <yangwen8701@126.com>
2016-08-13 15:13:18 +0800

    张三：提交MainActivity，在MainActivity中添加了一段代码

--- app/src/main/java/com/androidxx/yangjw/android1604toplevel/MainActivity.java
+++ app/src/main/java/com/androidxx/yangjw/android1604toplevel/MainActivity.java
@@ -3,11 +3,15 @@ package com.androidxx.yangjw.android1604toplevel;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Git的使用
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //zhangsan
     }
 }
